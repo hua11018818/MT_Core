552742b1ebebcdf81d2d995005343063a213bb7a
hua11018818 <87325075@qq.com>
2016-11-21 19:49:12 +0800

    Create

---
+++ Core/MT.Core/Document/JsonHelper.cs
@@ -0,0 +1,32 @@
+﻿/******************************************************
+ * 项目名称: MT.Core.Document
+ * 项目描述:
+ * 类 名 称: JsonHelper
+ * 版 本 号:
+ * 说    明:
+ * 作    者：唐明华
+ * 创建时间：2016/11/21 19:41:28
+ *******************************************************
+ * 更新时间: 2016/11/21 19:41:28
+******************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.Core.Document
+{
+    /// <summary>
+    /// 请引用Newtonsoft.Json类库去操作JSON数据
+    ///1,序列号JSON字符串
+    ///string jsonStr = JsonConvert.SerializeObject(obj);
+    ///2，通过字符串反射对象
+    ///object obj = JsonConvert.DeserializeObject<object>(jsonStr);
+    ///3,拼接JSON对象推荐使用：JObject
+    ///4,拼接JSON数据推荐使用：JArray
+    /// </summary>
+    public class JsonHelper
+    {
+    }
+}
---
+++ Core/MT.Core/Document/XmlHelper.cs
@@ -0,0 +1,37 @@
+﻿/******************************************************
+ * 项目名称: MT.Core.Document
+ * 项目描述:
+ * 类 名 称: XmlHelper
+ * 版 本 号:
+ * 说    明:
+ * 作    者：唐明华
+ * 创建时间：2016/11/21 19:42:54
+ *******************************************************
+ * 更新时间: 2016/11/21 19:42:54
+******************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MT.Core.Document
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class XmlHelper
+    {
+        /// <summary>
+        /// 判断XML节点中是否包含子节点
+        /// </summary>
+        /// <param name="xElement">节点</param>
+        /// <param name="name">子节点名称</param>
+        /// <returns></returns>
+        public static bool HasElementsByName(XElement xElement, string name)
+        {
+            return null != xElement && xElement.Elements(name).Any();
+        }
+    }
+}
